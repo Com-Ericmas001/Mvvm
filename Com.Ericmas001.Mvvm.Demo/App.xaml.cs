11cb1cd4d28b11745ba6272e5b57d0540aff575d
Éric Massé <Ericmas001@gmail.com>
2019-06-02 10:29:02 -0400

    Adding TextBoxScrollToEndExtension

--- Com.Ericmas001.Mvvm.Demo/App.xaml.cs
+++ Com.Ericmas001.Mvvm.Demo/App.xaml.cs
@@ -1,17 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows;
-
-namespace Com.Ericmas001.Mvvm.Demo
+﻿namespace Com.Ericmas001.Mvvm.Demo
 {
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
-    public partial class App : Application
+    public partial class App
     {
     }
 }
---
+++ Com.Ericmas001.Mvvm/ControlExtensions/TextBoxScrollToEndExtension.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Com.Ericmas001.Mvvm.ControlExtensions
+{
+    /// <summary>
+    /// TextBoxScrollToEndExtension
+    /// </summary>
+    public static class TextBoxScrollToEndExtension
+    {
+        /// <summary>
+        /// AlwaysScrollToEndProperty
+        /// </summary>
+        public static readonly DependencyProperty AlwaysScrollToEndProperty = DependencyProperty.RegisterAttached("AlwaysScrollToEnd",
+                                                                                                                  typeof(bool),
+                                                                                                                  typeof(TextBoxScrollToEndExtension),
+                                                                                                                  new PropertyMetadata(false, AlwaysScrollToEndChanged));
+
+        private static void AlwaysScrollToEndChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            if (sender is TextBox tb)
+            {
+                bool alwaysScrollToEnd = (e.NewValue != null) && (bool)e.NewValue;
+                if (alwaysScrollToEnd)
+                {
+                    tb.ScrollToEnd();
+                    tb.TextChanged += TextChanged;
+                }
+                else
+                    tb.TextChanged -= TextChanged;
+            }
+            else
+                throw new InvalidOperationException("The attached AlwaysScrollToEnd property can only be applied to TextBox instances.");
+        }
+
+        /// <summary>
+        /// GetAlwaysScrollToEnd
+        /// </summary>
+        public static bool GetAlwaysScrollToEnd(TextBox textBox)
+        {
+            if (textBox == null) throw new ArgumentNullException(nameof(textBox));
+
+            return (bool)textBox.GetValue(AlwaysScrollToEndProperty);
+        }
+
+        /// <summary>
+        /// SetAlwaysScrollToEnd
+        /// </summary>
+        public static void SetAlwaysScrollToEnd(TextBox textBox, bool alwaysScrollToEnd)
+        {
+            if (textBox == null) throw new ArgumentNullException(nameof(textBox));
+
+            textBox.SetValue(AlwaysScrollToEndProperty, alwaysScrollToEnd);
+        }
+
+        private static void TextChanged(object sender, TextChangedEventArgs e)
+        {
+            ((TextBox)sender).ScrollToEnd();
+        }
+    }
+}
