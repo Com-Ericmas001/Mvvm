014cf2b2750df88a3304366a3f0c6b4b966902f9
Éric Massé <Ericmas001@gmail.com>
2019-11-17 14:35:39 -0500

    Version 7.0

---
+++ Com.Ericmas001.MvvmCore/MvvmCoreRegistrant.cs
@@ -0,0 +1,13 @@
+﻿using Com.Ericmas001.DependencyInjection.Registrants;
+
+namespace Com.Ericmas001.MvvmCore
+{
+    public class MvvmCoreRegistrant : AbstractRegistrant
+    {
+        public WindowCreator WindowCreator { get; } = new WindowCreator();
+        protected override void RegisterEverything()
+        {
+            RegisterInstance<IWindowCreator>(WindowCreator);
+        }
+    }
+}
