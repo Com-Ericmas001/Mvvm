04f80de7270ba764a3b0bdbb2ee6499b1449b6dd
Éric Massé <Ericmas001@gmail.com>
2019-11-19 09:38:30 -0500

    More possibilities TEVM

--- Com.Ericmas001.Mvvm.Shared/ViewModels/TreeElementViewModel.cs
+++ Com.Ericmas001.Mvvm.Shared/ViewModels/TreeElementViewModel.cs
@@ -8,6 +8,17 @@ using Com.Ericmas001.Mvvm.Collections;
 
 namespace Com.Ericmas001.Mvvm.ViewModels
 {
+    public abstract class TreeElementViewModel<TBaseElementViewModel,TModel> : AbstractTreeElementViewModel<TBaseElementViewModel>
+        where TBaseElementViewModel : AbstractTreeElementViewModel<TBaseElementViewModel> 
+        where TModel : class
+    {
+        public new TModel Model { get; private set; }
+        public virtual void Init(TBaseElementViewModel parent, TModel model)
+        {
+            base.Init(parent, model);
+            Model = model;
+        }
+    }
     public abstract class TreeElementViewModel<T> : TreeElementViewModel where T : class
     {
         public new T Model { get; private set; }
@@ -17,7 +28,58 @@ namespace Com.Ericmas001.Mvvm.ViewModels
             Model = model;
         }
     }
-    public abstract class TreeElementViewModel : ViewModelBase
+    public abstract class TreeElementViewModel : AbstractTreeElementViewModel<TreeElementViewModel>
+    {
+
+    }
+    public abstract class AbstractTreeElementViewModel<TBaseElementViewModel> : SuperBasicTreeElementViewModel where TBaseElementViewModel : AbstractTreeElementViewModel<TBaseElementViewModel>
+    {
+        public FastObservableCollection<TBaseElementViewModel> Children { get; } = new FastObservableCollection<TBaseElementViewModel>();
+
+        public TBaseElementViewModel Parent { get; private set; }
+        public bool HasOnlyOneLeaf => NbLeaves == 1;
+        public AbstractTreeElementViewModel<TBaseElementViewModel> FirstLeaf => TreeLeaves.FirstOrDefault();
+        public bool HasChildren => Children.Any();
+        public int NbChildren => Children.Count;
+        public int NbLeaves => HasChildren ? TreeLeaves.Count() : 0;
+        public bool HasGrandChildren => HasChildren && Children.Any(x => x.HasChildren);
+        public bool CanExpand => HasChildren && !IsExpanded;
+        public bool CanCollapse => HasChildren && IsExpanded;
+
+
+        public virtual void Init(TBaseElementViewModel parent, object model)
+        {
+            base.Init(model);
+            Parent = parent;
+        }
+        public void ExpandAll()
+        {
+            Children.ToList().ForEach(x => x.ExpandAll());
+            Expand();
+        }
+        public void ExpandWithParents()
+        {
+            Expand();
+            Parent?.ExpandWithParents();
+        }
+        public void CollapseAll()
+        {
+            Collapse();
+            Children.ToList().ForEach(x => x.CollapseAll());
+        }
+        public virtual IEnumerable<AbstractTreeElementViewModel<TBaseElementViewModel>> TreeLeaves
+        {
+            get { return HasChildren ? Children.SelectMany(x => x.TreeLeaves) : new [] { this }; }
+        }
+
+        public virtual void AddChildren(params TBaseElementViewModel[] elems)
+        {
+            if (elems.Any())
+                Children.AddItems(elems);
+        }
+
+    }
+    public abstract class SuperBasicTreeElementViewModel : ViewModelBase
     {
         private bool _isExpanded;
 
@@ -65,61 +127,21 @@ namespace Com.Ericmas001.Mvvm.ViewModels
         protected virtual void OnBeforeCollapse()
         {
         }
-
-        public FastObservableCollection<TreeElementViewModel> Children { get; } = new FastObservableCollection<TreeElementViewModel>();
-
-        public TreeElementViewModel Parent { get; private set; }
         public object Model { get; private set; }
         public abstract string Text { get; }
-        public bool HasOnlyOneLeaf => NbLeaves == 1;
-        public TreeElementViewModel FirstLeaf => TreeLeaves.FirstOrDefault();
-        public bool HasChildren => Children.Any();
-        public int NbChildren => Children.Count;
-        public int NbLeaves => HasChildren ? TreeLeaves.Count() : 0;
-        public bool HasGrandChildren => HasChildren && Children.Any(x => x.HasChildren);
-        public bool CanExpand => HasChildren && !IsExpanded;
-        public bool CanCollapse => HasChildren && IsExpanded;
 
 
-        public virtual void Init(TreeElementViewModel parent, object model)
+        public void Init(object model)
         {
-            Parent = parent;
             Model = model;
         }
         public void Expand()
         {
             IsExpanded = true;
         }
-        public void ExpandAll()
-        {
-            Children.ToList().ForEach(x => x.ExpandAll());
-            Expand();
-        }
-        public void ExpandWithParents()
-        {
-            Expand();
-            Parent?.ExpandWithParents();
-        }
         public void Collapse()
         {
             IsExpanded = false;
         }
-
-        public void CollapseAll()
-        {
-            Collapse();
-            Children.ToList().ForEach(x => x.CollapseAll());
-        }
-        public virtual IEnumerable<TreeElementViewModel> TreeLeaves
-        {
-            get { return HasChildren ? Children.SelectMany(x => x.TreeLeaves) : new[] { this }; }
-        }
-
-        public virtual void AddChildren(params TreeElementViewModel[] elems)
-        {
-            if (elems.Any())
-                Children.AddItems(elems);
-        }
-
     }
 }
